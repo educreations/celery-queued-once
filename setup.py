a1df08e3373d179544b0957fe2c6ecb1379eede4
Chris Streeter <chris@educreations.com>
2020-01-10 10:26:16 -0800

    Additional testing

--- setup.py
+++ setup.py
@@ -6,18 +6,18 @@ import sys
 from setuptools import setup
 
 
-if sys.argv[-1] == 'publish':
-    os.system('python setup.py register sdist bdish_wheel upload')
+if sys.argv[-1] == "publish":
+    os.system("python setup.py register sdist bdish_wheel upload")
     sys.exit()
 
 
 setup(
-    name='celery-queued-once',
-    version='0.2',
-    description='Celery base task de-duplicating tasks',
-    author='Educreations Engineering',
-    author_email='engineering@educreations.com',
-    url='https://github.com/educreations/celery-queued-once',
-    packages=['queued_once'],
-    install_requires=['celery', 'django >= 1.7'],
+    name="celery-queued-once",
+    version="0.2",
+    description="Celery base task de-duplicating tasks",
+    author="Educreations Engineering",
+    author_email="engineering@educreations.com",
+    url="https://github.com/educreations/celery-queued-once",
+    packages=["queued_once"],
+    install_requires=["celery>=3.1.17,<4.0", "Django>=1.7.4,<1.10"],
 )
